131df182e02bcf9ad34a487bfd2a3d2fcdb638fc
Alex090290 <raguilar@gamaconsumibles.com.mx>
2024-02-11 13:20:05 -0600

    se coloca logotipo en main

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <div
             style={{ height: "90vh", overflowY: "auto" }}
             id="pages-container"
-            className="container-fluid mt-1"
+            className="container-fluid mt-1 main-logo"
           >
             {children}
           </div>
