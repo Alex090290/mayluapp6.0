f1a19a914f21d72a4a8cbaed9b8dc8f6a23d2c64
Alex090290 <raguilar@gamaconsumibles.com.mx>
2024-02-24 22:33:47 -0600

    se cambia el dropdown de nav

--- components/navigation/items/ItemCatalogos.tsx
+++ components/navigation/items/ItemCatalogos.tsx
@@ -1,34 +1,43 @@
 "use client";
 
 import Link from "next/link";
-import { NavDropdown } from "react-bootstrap";
+import { Button, NavDropdown } from "react-bootstrap";
 import { FaWarehouse, FaTruck, FaRegUserCircle } from "react-icons/fa";
 import { BsBoxes, BsPersonLinesFill } from "react-icons/bs";
 
 function ItemCatalogos() {
   return (
-    <NavDropdown title="Catálogos">
-      <Link href="/almacenes" className="dropdown-item">
-        <FaWarehouse className="me-2" />
-        Almacenes
-      </Link>
-      <Link href="/productos" className="dropdown-item">
-        <BsBoxes className="me-2" />
-        Productos
-      </Link>
-      <Link href="/clientes" className="dropdown-item">
-        <BsPersonLinesFill className="me-2" />
-        Clientes
-      </Link>
-      <Link href="/proveedores" className="dropdown-item">
-        <FaTruck className="me-2" />
-        Proveedores
-      </Link>
-      <Link href="/operadores" className="dropdown-item">
-        <FaRegUserCircle className="me-2" />
-        Operadores
-      </Link>
-    </NavDropdown>
+    <div className="nav-item dropdown">
+      <button
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+        className="nav-link dropdown-toggle"
+      >
+        Catálogos
+      </button>
+      <div className="dropdown-menu">
+        <Link href="/almacenes" className="dropdown-item">
+          <FaWarehouse className="me-2" />
+          Almacenes
+        </Link>
+        <Link href="/productos" className="dropdown-item">
+          <BsBoxes className="me-2" />
+          Productos
+        </Link>
+        <Link href="/clientes" className="dropdown-item">
+          <BsPersonLinesFill className="me-2" />
+          Clientes
+        </Link>
+        <Link href="/proveedores" className="dropdown-item">
+          <FaTruck className="me-2" />
+          Proveedores
+        </Link>
+        <Link href="/operadores" className="dropdown-item">
+          <FaRegUserCircle className="me-2" />
+          Operadores
+        </Link>
+      </div>
+    </div>
   );
 }
 
