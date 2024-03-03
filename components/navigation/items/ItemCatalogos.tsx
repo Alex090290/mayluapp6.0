1fdba8d41763ab89dccca5ae81a9f1dd2acb68eb
Alex090290 <raguilar@gamaconsumibles.com.mx>
2024-03-03 13:41:19 -0600

    se modifica el itemnavcatalogo

--- components/navigation/items/ItemCatalogos.tsx
+++ components/navigation/items/ItemCatalogos.tsx
@@ -16,23 +16,23 @@ function ItemCatalogos() {
         </>
       }
     >
-      <Link href="/almacenes" className="dropdown-item">
+      <Link href="/catalogos/almacenes" className="dropdown-item">
         <FaWarehouse className="me-2" />
         Almacenes
       </Link>
-      <Link href="/productos" className="dropdown-item">
+      <Link href="/catalogos//productos" className="dropdown-item">
         <BsBoxes className="me-2" />
         Productos
       </Link>
-      <Link href="/clientes" className="dropdown-item">
+      <Link href="/catalogos//clientes" className="dropdown-item">
         <BsPersonLinesFill className="me-2" />
         Clientes
       </Link>
-      <Link href="/proveedores" className="dropdown-item">
+      <Link href="/catalogos//proveedores" className="dropdown-item">
         <FaTruck className="me-2" />
         Proveedores
       </Link>
-      <Link href="/operadores" className="dropdown-item">
+      <Link href="/catalogos//operadores" className="dropdown-item">
         <FaRegUserCircle className="me-2" />
         Operadores
       </Link>
